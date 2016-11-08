9d905846c7813d8c1c5453a39dc61be93be1666a
Jasy Dong <jasydong@qq.com>
2016-11-08 15:33:15 +0800

    兼容HTML数据格式以及优化显示界面

--- get.php
+++ get.php
@@ -1,10 +1,13 @@
 <?php
 
 $apiUrl = isset($_GET['api_url']) ? $_GET['api_url']:"";
+if (isset($_GET['api_url'])) {
+	unset($_GET['api_url']);
+}
 
 if (!empty($apiUrl)) {
-	$result = curl_get($apiUrl);
-	echo htmlspecialchars($result);
+	$result = curl_get($apiUrl, $_GET);
+	echo htmlspecialchars($result, ENT_NOQUOTES);
 }
 
 
@@ -19,27 +22,27 @@ if (!empty($apiUrl)) {
 * @param array $options for cURL 
 * @return string 
 */ 
-function curl_post($url, array $post = NULL, array $options = array()) 
-{ 
-    $defaults = array( 
-        CURLOPT_POST => 1, 
-        CURLOPT_HEADER => 0, 
-        CURLOPT_URL => $url, 
-        CURLOPT_FRESH_CONNECT => 1, 
-        CURLOPT_RETURNTRANSFER => 1, 
-        CURLOPT_FORBID_REUSE => 1, 
-        CURLOPT_TIMEOUT => 4, 
-        CURLOPT_POSTFIELDS => http_build_query($post) 
-    ); 
+function curl_post($url, array $post = NULL, array $options = array()) {
+    $defaults = array(
+        CURLOPT_POST => 1,
+        CURLOPT_HEADER => 0,
+        CURLOPT_URL => $url,
+        CURLOPT_FRESH_CONNECT => 1,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_FORBID_REUSE => 1,
+        CURLOPT_TIMEOUT => 4,
+        CURLOPT_POSTFIELDS => http_build_query($post)
+    );
 
-    $ch = curl_init(); 
-    curl_setopt_array($ch, ($options + $defaults)); 
-    if( ! $result = curl_exec($ch)) 
-    { 
-        trigger_error(curl_error($ch)); 
-    } 
-    curl_close($ch); 
-    return $result; 
+    $ch = curl_init();
+    curl_setopt_array($ch, ($options + $defaults));
+    if( ! $result = curl_exec($ch)) {
+        trigger_error(curl_error($ch));
+    }
+
+    curl_close($ch);
+
+    return $result;
 } 
 
 /** 
