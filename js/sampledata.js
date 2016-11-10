740b19f2c36d193a60175a2345f07aa4c5812271
Jasy Dong <jasydong@qq.com>
2016-11-10 17:58:17 +0800

    添加样例数据

---
+++ js/sampledata.js
@@ -0,0 +1,6 @@
+/**
+ * sample data
+ */
+
+var sampleHistoryData = [{"title":"system.getLevelInfoByGameV2","url":"http://192.168.200.21/dfqp/?action=system.getLevelInfoByGameV2&game_id=1802"}];
+var sampleEnvSettings = [{"name":"测试环境","url":"http://192.168.200.21/dfqp/"}];
